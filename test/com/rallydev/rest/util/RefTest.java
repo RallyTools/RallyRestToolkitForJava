84cb48ca3642d11ca37a58061af5da8ab1216306
Kyle Morse <kyle.r.morse@gmail.com>
2012-10-06 10:37:45 -0600

    v1.03: Add support for dynatypes

--- test/com/rallydev/rest/util/RefTest.java
+++ test/com/rallydev/rest/util/RefTest.java
@@ -14,6 +14,14 @@ public class RefTest {
     }
 
     @Test
+    public void shouldDetectValidDynatypeRefs() {
+        Assert.assertTrue(Ref.isRef("/portfolioitem/feature/1234"), "Valid relative ref");
+        Assert.assertTrue(Ref.isRef("/portfolioitem/feature/1234.js"), "Valid relative ref w/ extension");
+        Assert.assertTrue(Ref.isRef("https://rally1.rallydev.com/slm/webservice/1.32/portfolioitem/feature/1234"), "Valid absolute ref");
+        Assert.assertTrue(Ref.isRef("http://rally1.rallydev.com/slm/webservice/1.32/portfolioitem/feature/1234.js"), "Valid absolute ref w/ extension");
+    }
+
+    @Test
     public void shouldDetectInvalidRefs() {
         Assert.assertFalse(Ref.isRef("/defect"), "Invalid ref");
         Assert.assertFalse(Ref.isRef("https://rally1.rallydev.com/slm/webservice/1.32/defect/abc.js"), "Invalid ref");
@@ -26,6 +34,13 @@ public class RefTest {
         Assert.assertEquals(Ref.getRelativeRef("/defect/1234"), "/defect/1234", "Already relative ref");
         Assert.assertEquals(Ref.getRelativeRef("/defect/1234.js"), "/defect/1234", "Already relative ref");
         Assert.assertEquals(Ref.getRelativeRef("https://rally1.rallydev.com/slm/webservice/1.32/defect/1234"), "/defect/1234", "Absolute ref");
+    }
+
+    @Test
+    public void shouldReturnValidDynatypeRelativeRefs() {
+        Assert.assertEquals(Ref.getRelativeRef("/portfolioitem/feature/1234"), "/portfolioitem/feature/1234", "Already relative ref");
+        Assert.assertEquals(Ref.getRelativeRef("/portfolioitem/feature/1234.js"), "/portfolioitem/feature/1234", "Already relative ref");
+        Assert.assertEquals(Ref.getRelativeRef("https://rally1.rallydev.com/slm/webservice/1.32/portfolioitem/feature/1234"), "/portfolioitem/feature/1234", "Absolute ref");
     }
 
     @Test
@@ -43,6 +58,13 @@ public class RefTest {
     }
 
     @Test
+    public void shouldReturnTypesFromDynatypeRefs() {
+        Assert.assertEquals(Ref.getTypeFromRef("/portfolioitem/feature/1234"), "portfolioitem/feature", "Relative ref");
+        Assert.assertEquals(Ref.getTypeFromRef("/portfolioitem/feature/1234.js"), "portfolioitem/feature", "Relative ref with extension");
+        Assert.assertEquals(Ref.getTypeFromRef("https://rally1.rallydev.com/slm/webservice/1.32/portfolioitem/feature/1234"), "portfolioitem/feature", "Valid absolute ref");
+    }
+
+    @Test
     public void shouldReturnNullTypesFromRefs() {
         Assert.assertNull(Ref.getTypeFromRef("blah"), "Not a ref");
         Assert.assertNull(Ref.getTypeFromRef(""), "Empty ref");
@@ -54,6 +76,13 @@ public class RefTest {
         Assert.assertEquals(Ref.getOidFromRef("/defect/1234"), "1234", "Relative ref");
         Assert.assertEquals(Ref.getOidFromRef("/defect/1234.js"), "1234", "Relative ref with extension");
         Assert.assertEquals(Ref.getOidFromRef("https://rally1.rallydev.com/slm/webservice/1.32/defect/1234"), "1234", "Valid absolute ref");
+    }
+
+    @Test
+    public void shouldReturnOidsFromDynatypeRefs() {
+        Assert.assertEquals(Ref.getOidFromRef("/portfolioitem/feature/1234"), "1234", "Relative ref");
+        Assert.assertEquals(Ref.getOidFromRef("/portfolioitem/feature/1234.js"), "1234", "Relative ref with extension");
+        Assert.assertEquals(Ref.getOidFromRef("https://rally1.rallydev.com/slm/webservice/1.32/portfolioitem/feature/1234"), "1234", "Valid absolute ref");
     }
     
     @Test
